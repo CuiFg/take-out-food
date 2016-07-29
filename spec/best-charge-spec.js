3743e29b490af1223d3df5523f8f9059d032a4a4
zhaocui <CuiFg>
2016-07-29 13:26:22 +0800

    take-out-food

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -46,3 +46,317 @@ describe('Take out food', function () {
   });
 
 });
+
+describe("parseSelectedItems",function(){
+  it("parseSelectedItems", function(){
+    let selectedItems = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+    let result = parseSelectedItems(selectedItems);
+    let parsedItems = [
+      {
+        id: "ITEM0001 ",
+        count:1
+      },
+      {
+        id: "ITEM0013 ",
+        count:2
+      },
+      {
+        id: "ITEM0022 ",
+        count:1
+      }
+    ];
+    expect(result).toEqual(parsedItems)
+  })
+
+});
+
+describe("getCartItemsCount",function(){
+  it("getCartItemsCount", function(){
+    let parsedItems = [
+      {
+        id: "ITEM0001",
+        count:1
+      },
+      {
+        id: "ITEM0013",
+        count: 2
+      },
+      {
+        id: "ITEM0022",
+        count: 1
+      }
+    ];
+    let Items = loadAllItems();
+    let result = getCartItemsCount(Items,parsedItems);
+    let cartItemsCount = [
+      {
+        id: 'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        count:1
+
+      },
+      {
+        id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        count:2
+      },
+      {
+        id: 'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        count:1
+      }];
+    expect(result).toEqual(cartItemsCount);
+  })
+});
+
+describe("getCartItemsCountType", function(){
+  it("getCartItemsCountType", function(){
+    let promotionItems = loadPromotions();
+    let cartItemsCount = [
+      {
+        id: 'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        count:1
+
+      },
+      {
+        id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        count:2
+      },
+      {
+        id: 'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        count:1
+      }];
+    let result = getCartItemsCountType(cartItemsCount,promotionItems);
+    let cartItemsCountType = [
+      {
+        id: 'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        count:1,
+        type:'指定菜品半价'
+      },
+      {
+        id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        count:2,
+        type:'满30减6元'
+      },
+      {
+        id: 'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        count:1,
+        type:'指定菜品半价'
+      }
+    ];
+    expect(result).toEqual(cartItemsCountType);
+  })
+});
+
+describe("getPromotionItems",function(){
+  it("getPromotionItems", function(){
+    let cartItemsCountType = [
+      {
+        id: 'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        count:1,
+        type:'指定菜品半价'
+      },
+      {
+        id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        count:2,
+        type:'满30减6元'
+      },
+      {
+        id: 'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        count:1,
+        type:'指定菜品半价'
+      }];
+
+    let result  = getPromotionItems(cartItemsCountType);
+    let cartItemsCountTypePromotion = [
+      {
+        id: 'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        count:  1,
+        type:'指定菜品半价',
+        promotionPrice:9.00,
+        promotionedPrice:9.00
+      },
+      {
+        id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        count:2,
+        type:'满30减6元',
+        //promotionPrice:0.00,
+        //promotionedPrice:6.00
+      },
+      {
+        id: 'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        count:1,
+        type:'指定菜品半价',
+        promotionPrice:4,
+        promotionedPrice:4
+      }];
+    expect(result).toEqual(cartItemsCountTypePromotion)
+  })
+
+});
+
+describe("getSubTotal", function() {
+  it("getSubTotal",function(){
+    let cartItemsCountTypePromotion = [
+      {
+        id: 'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        count:1,
+        type:'指定菜品半价',
+        promotionPrice:9.00,
+        promotionedPrice:9.00
+      },
+      {
+        id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        count:2,
+        type:'满30减6元'
+      },
+      {
+        id: 'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        count:1,
+        type:'指定菜品半价',
+        promotionPrice:4,
+        promotionedPrice:4
+      }];
+    let result= getSubTotal(cartItemsCountTypePromotion);
+    let subTotalItems = [
+      {
+        id: 'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        count:1,
+        type:'指定菜品半价',
+        promotionPrice:9.00,
+        promotionedPrice:9.00,
+        subTotal:18.00
+      },
+      {
+        id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        count:2,
+        type:'满30减6元',
+        subTotal:12.00
+      },
+      {
+        id: 'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        count:1,
+        type:'指定菜品半价',
+        promotionPrice:4,
+        promotionedPrice:4,
+        subTotal:8.00
+      }];
+    expect(result).toEqual(subTotalItems);
+  })
+
+});
+
+describe("getSaveMoney",function(){
+  it("getSaveMoney", function(){
+    let subTotalItems = [
+      {
+        id: 'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        count:1,
+        type:'指定菜品半价',
+        promotionPrice:9.00,
+        promotionedPrice:9.00,
+        subTotal:18.00
+      },
+      {
+        id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        count:2,
+        type:'满30减6元',
+        subTotal:12.00
+      },
+      {
+        id: 'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        count:1,
+        type:'指定菜品半价',
+        promotionPrice:4,
+        promotionedPrice:4,
+        subTotal:8.00
+      }];
+    let result = getSaveMoney(cartItemsCountTypePromotion);
+    let saveMoney = 13.00;
+    expect(result).toEqual(saveMoney);
+  })
+});
+
+describe("getTotal", function(){
+  it("getTotal",function(){
+    let subTotalItems = [
+      {
+        id: 'ITEM0001',
+        name: '黄焖鸡',
+        price: 18.00,
+        count:1,
+        type:'指定菜品半价',
+        promotionPrice:9.00,
+        promotionedPrice:9.00,
+        subTotal:18.00
+      },
+      {
+        id: 'ITEM0013',
+        name: '肉夹馍',
+        price: 6.00,
+        count:2,
+        type:'满30减6元',
+        subTotal:12.00
+      },
+      {
+        id: 'ITEM0022',
+        name: '凉皮',
+        price: 8.00,
+        count:1,
+        type:'指定菜品半价',
+        promotionPrice:4,
+        promotionedPrice:4,
+        subTotal:8.00
+      }];
+    let result = getTotal(cartItemsCountTypePromotion);
+    let total = 25.00;
+    expect(result).toEqual(total);
+  })
+});
+
+
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,122 @@
-function bestCharge(selectedItems) {
-  return /*TODO*/;
+function bestCharge(selectedItems)
+{
+  //return /*TODO*/;
+
+  /*let  parsedItems =parseSelectedItems(selectedItems);
+   let Items = loadAllItems();
+   let cartItemsCount = getCartItemsCount(Items,parsedItems);
+   let promotionItems = loadPromotions();
+   let cartItemsCountType = getCartItemsCountType(cartItemsCount,promotionItems);
+   let cartItemsCountTypePromotion = getPromotionItems(cartItemsCountType);
+
+   let subTotalItems = getSubTotal(cartItemsCountTypePromotion);
+   let saveMoney = getSaveMoney(cartItemsCountTypePromotion);
+   let total = getTotal(cartItemsCountTypePromotion);
+*/
+
+}
+
+function parseSelectedItems(selectedItems)
+{
+  return selectedItems.map(function(selectedItem){
+    let selectedItemsPart = selectedItem.split("x");
+    return {id: selectedItemsPart[0], count: parseFloat(selectedItemsPart[1]) || 1 };
+  })
+}
+
+function getCartItemsCount(Items,parsedItems)
+{
+  let cartItemsCount =[];
+  for(let i =0; i< parsedItems.length; i++)
+  {
+    for(let j=0; j< Items.length; j++)
+    {
+      if (parsedItems[i].id === Items[j].id)
+      {
+        cartItemsCount.push(Object.assign({},Items[j],{count:parsedItems[i].count}));
+      }
+    }
+  }
+  return  cartItemsCount;
+}
+
+function getCartItemsCountType(cartItemsCount,promotionItems)
+{
+  let cartItemsCountType = [];
+//  let sameType=[];
+  for (let m = 0; m < cartItemsCount.length; m++)
+  {
+     cartItemsCountType.push(Object.assign({}, cartItemsCount[m], {type: '满30减6元'}));
+  }
+  for (let n = 0; n < cartItemsCountType.length; n++)
+  {
+
+    for(let i = 0; i < promotionItems.length; i++)
+    {
+      if(promotionItems[i].type === '指定菜品半价')
+      {
+        for (let j = 0; j < promotionItems[i].items.length; j++)
+        {
+          if (cartItemsCountType[n].id === promotionItems[i].items[j])
+          {
+            cartItemsCountType[n].type = '指定菜品半价';
+          }
+
+        }
+      }
+    }
+  }
+  return cartItemsCountType;
+}
+
+function getPromotionItems(cartItemsCountType)
+{
+  let cartItemsCountTypePromotion = [];
+  for(let i=0; i < cartItemsCountType.length; i++)
+  {
+    if(cartItemsCountType[i].type === '指定菜品半价' )
+    {
+      let promotionPrice = parseFloat(cartItemsCountType[i].price / 2);
+      let promotionedPrice = parseFloat(cartItemsCountType[i].price / 2);
+      cartItemsCountTypePromotion.push(Object.assign({},cartItemsCountType[i],
+        {promotionPrice:promotionPrice, promotionedPrice: promotionedPrice}));
+    }
+    if(cartItemsCountType[i].type === '满30减6元')
+    {
+      cartItemsCountTypePromotion.push(Object.assign({},cartItemsCountType[i]));
+    }
+  }
+  return cartItemsCountTypePromotion;
+}
+
+function  getSubTotal(cartItemsCountTypePromotion)
+{
+  let subTotalItems =[];
+  for(let i=0; i < cartItemsCountTypePromotion.length; i++)
+  {
+    let subTotal = cartItemsCountTypePromotion[i].price *  cartItemsCountTypePromotion[i].count;
+    subTotalItems.push(Object.assign({},cartItemsCountTypePromotion[i],{subTotal: subTotal} ));
+  }
+  return subTotalItems;
+}
+
+function getSaveMoney(subTotalItems)
+{
+  let saveMoney = 0;
+  for(let i = 0; i < subTotalItems.length; i++ )
+  {
+    if(subTotalItems[i].type === "指定菜品半价")
+    saveMoney += subTotalItems[i].promotionPrice * subTotalItems[i].count;
+  }
+  return saveMoney;
+}
+
+function getTotal(subTotalItems)
+{
+  let total = 0;
+  for(let i = 0; i < subTotalItems.length; i++ )
+  {
+    total += subTotalItems[i].promotionedPrice;
+  }
+  return total;
 }
